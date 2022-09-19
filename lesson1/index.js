249a073fffec9a9012a866fd7c0cb07f4154c764
Anrey <andrey@example.com>
2022-09-19 21:52:08 +0300

    first commit in branch lesson1

---
+++ lesson1/index.js
@@ -0,0 +1,15 @@
+let string = 'jfkjdfdjkfjd'
+let number = 123
+let boolean = false
+let un = undefined
+let nul = null
+
+console.log('first', un)
+un = 1
+console.log('second', un)
+
+const obj = {p: 3.14}
+console.log(obj, obj.p)
+obj.p = 3
+obj.e = 2
+console.log(obj, obj.p)
