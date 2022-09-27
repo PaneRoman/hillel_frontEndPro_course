0d01a07fee1b2f571725bf35d3879580210b0c98
Anrey <andrey@example.com>
2022-09-28 01:58:23 +0300

    edit lesson2.html

---
+++ lessons/lesson2.js
@@ -0,0 +1,123 @@
+// let a = 10;
+// const b = 15;
+
+// console.log(--a);
+// console.log(a);
+
+
+// let test = 'wer';
+// let test2 = 'jfdjf';
+
+// console.log(`${test}${test2}`); //werjfdjf
+
+
+
+// let a = 10;
+// let test = 'dfjdkf';
+
+// console.log(a + test); //10dfjdkf
+
+
+
+// console.log(Number(true)); // 1
+// console.log(Number('123')); //123
+// console.log(Number('jhjkghjkd')); //NaN
+
+
+// function test () {
+
+// };
+
+// const test = function () {
+
+// };
+
+
+// function isEven (numb, numb2) {
+//     return numb*2 + numb2
+// };
+
+// console.log(isEven(10, 2, 3));
+
+
+// const multiple = 5;
+
+// function isEven2 (numb) {
+
+//     function test () {
+//         console.log('test');
+//     };
+
+//     test();
+
+//     return numb*multiple
+// };
+
+// console.log(isEven2(10, 2, 3));
+
+
+
+// const test = 'asf';
+
+// console.log(test[2]); //f
+
+
+// const obj = {};
+
+// obj.p = 'te';
+
+// console.log(obj);
+
+
+
+// ! - отрицания
+// || - ИЛИ
+// && - И
+
+
+// console.log(!false);
+// console.log(!true);
+
+// console.log(!!5); // 5 is true => !!true => !false => true
+// console.log(!undefined); // true
+// console.log(!NaN); // true
+// undefined, NaN, null, 0, '' - falsy
+
+
+
+// console.log(true && false); // false
+// console.log(true && true); // true
+// console.log(false && false); // false
+
+// console.log(true || false); // true
+// console.log(true || true); // true
+// console.log(false || false); // false
+
+
+// const a = 0;
+// if (!{}) {
+//     console.log('dkjfksjd')
+// } else {
+//     console.log('333333')
+// }
+
+
+// const a = true;
+// const b = false;
+// const c = true;
+
+// if (a || c && b) {
+//     console.log('if condition');
+// } else {
+//     console.log('else condition');
+// }
+
+
+
+
+
+
+
+
+
+
