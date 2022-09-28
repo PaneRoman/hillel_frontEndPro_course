bf389c3daca723194b55a8991b114ba9f79965e9
Anrey <andrey@example.com>
2022-09-28 18:21:20 +0300

    some commit

--- dz/dz2.js
+++
@@ -1,7 +0,0 @@
-let num1 = Number(prompt('Enter First number, please:', 10));
-let num2 = Number(prompt('Enter Second number, please:', 5));
-
-alert(`${num1}+${num2}=${num1 + num2}
-${num1}-${num2}=${num1 - num2}
-${num1}*${num2}=${num1 * num2}
-${num1}/${num2}=${num1 / num2}`);
