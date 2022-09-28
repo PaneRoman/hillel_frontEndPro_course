3cf48e850919ed46ffe50f990cba43040f97728c
Anrey <andrey@example.com>
2022-09-28 18:05:40 +0300

    add dz2.js and some code

---
+++ dz/dz2.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt('Enter First number, please:', 10));
+let num2 = Number(prompt('Enter Second number, please:', 5));
+
+alert(`${num1}+${num2}=${num1 + num2}
+${num1}-${num2}=${num1 - num2}
+${num1}*${num2}=${num1 * num2}
+${num1}/${num2}=${num1 / num2}`);
