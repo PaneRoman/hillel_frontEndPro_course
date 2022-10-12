9905e6d01137dc191c726fa91e3ccbd789ba90f3
Anrey <andrey@example.com>
2022-10-13 02:42:44 +0300

    edit lesson5_practic.js

--- lessons/lesson5_practic.js
+++ lessons/lesson5_practic.js
@@ -1,5 +1,5 @@
-const mainN = prompt('Введите длинну главного массива');
-const subN = prompt('Введите длинну внутренних массивов');
+// const mainN = prompt('Введите длинну главного массива');
+// const subN = prompt('Введите длинну внутренних массивов');
 
 // const arr = new Array(+mainN).fill(0)
 //     .map((_, ind) => new Array(+subN).fill(0)
@@ -7,15 +7,15 @@ const subN = prompt('Введите длинну внутренних масси
 
 // console.log(arr)
 
-let arr = [];
-for (let i = 0; i < mainN; i++) {
-    arr.push([]);
-    for (let j = 0; j < subN; j++) {
-        arr[i].push(Math.floor(Math.random() * (100 - 1)) + 1)
+// let arr = [];
+// for (let i = 0; i < mainN; i++) {
+//     arr.push([]);
+//     for (let j = 0; j < subN; j++) {
+//         arr[i].push(Math.floor(Math.random() * (100 - 1)) + 1)
         
-    }
-}
-console.log(arr)
+//     }
+// }
+// console.log(arr)
 
 
 
@@ -59,3 +59,28 @@ function deleteMin(arr) {
 console.log(deleteMin([1,2,3,4,5]));
 console.log(deleteMin([5,3,2,1,4]));
 console.log(deleteMin([2,2,1,2,1]));
+
+
+// Перевернуть каждое второе слово в заданной строке, а затем вернуть строку.
+// Отбросьте все начальные или конечные пробелы, убедившись, что между каждым словом есть ровно один пробел.
+// Знаки припинания следует рассматривать так, как если бы они были частью слова в этой кате.
+
+function reverseSecondWord(string) {
+    const dirtyWords = string.split(' ');
+    console.log(dirtyWords);
+    const words = dirtyWords.filter(function (item) {
+        return !!item;   // item приводится к boolean значению. через два знака !! item приводится к boolean значению более явно
+        
+    });
+    console.log(words)
+    
+    for (let i = 1; i < words.length; i+=2) {
+        words[i] = words[i].split('').reverse().join('');
+    }
+    console.log(words)
+
+    return words.join(' ');
+    
+}
+
+console.log(reverseSecondWord(' Я     люблю   учить            JavaScript. Он такой  же как  и EcmaScript '));
