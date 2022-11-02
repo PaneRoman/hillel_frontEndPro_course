1f210773e6945a6df85a0ef567c820f099735a2f
Anrey <andrey@example.com>
2022-11-02 04:21:23 +0200

    edit dz16

--- dz/dz16.js
+++ dz/dz16.js
@@ -1,63 +1,78 @@
-const SIZE_SMALL = {title: 'SMALL', coast: 50, cal: 20};
-const SIZE_LARGE = {title: 'LARGE', coast: 100, cal: 40};
-const STUFFING_CHEESE = {title: 'CHEESE', coast: 10, cal: 20};
-const STUFFING_SALAD = {title: 'SALAD', coast: 20, cal: 5};
-const STUFFING_POTATO = {title: 'POTATO', coast: 15, cal: 10};
-const TOPPING_SPICE = {title: 'SPICE', coast: 15, cal: 0};
-const TOPPING_MAYO = {title: 'MAYO', coast: 20, cal: 5};
-
-
- 
 class Hamburger {
-
-    #coast;
-    #cal;
 
     constructor(size, stuffing) {
         this.size = size.title;
         this.stuffing = stuffing.title;
-        this.#coast = size.coast + stuffing.coast;
-        this.#cal = size.cal + stuffing.cal;
+
+        this.coast = [size.coast, stuffing.coast];
+        this.cal = [size.cal, stuffing.cal];
+
     }
+
+    static SIZE_SMALL = {title: 'SIZE_SMALL', coast: 50, cal: 20};
+    static SIZE_LARGE = {title: 'SIZE_LARGE', coast: 100, cal: 40};
+    static STUFFING_CHEESE = {title: 'STUFFING_CHEESE', coast: 10, cal: 20};
+    static STUFFING_SALAD = {title: 'STUFFING_SALAD', coast: 20, cal: 5};
+    static STUFFING_POTATO = {title: 'STUFFING_POTATO', coast: 15, cal: 10};
+    static TOPPING_SPICE = {title: 'TOPPING_SPICE', coast: 15, cal: 0};
+    static TOPPING_MAYO = {title: 'TOPPING_MAYO', coast: 20, cal: 5};
 
     addTopping(topping) {
         if (!this.topping) {
             this.topping = [];
         }
+
         this.topping.push(topping.title);
-        this.#coast += topping.coast;
-        this.#cal += topping.cal;
+        this.coast.push(topping.coast);
+        this.cal.push(topping.cal);
     }
 
     removeTopping(topping) {
-        let index = this.topping.indexOf(topping.title);
-        this.topping.splice(index, 1);
-
-        this.#coast -= topping.coast;
-        this.#cal -= topping.cal;
-
-        return this.topping;
-    }
-
-    get coast() {
-        return this.#coast + 'tug';
-    }
-
-    get cal() {
-        return this.#cal + 'cal';
-    }
+        this.topping = this.topping.filter(item => !(item === topping.title));
+        this.coast = this.coast.filter(item => !(item === topping.coast));
+        this.cal = this.cal.filter(item => !(item === topping.cal));
         
+    
+        // this.topping.splice(index, 1);
+        // let index = this.topping.indexOf(topping.title);
+        // this.topping.splice(index, 1);
+        // this.#coast -= topping.coast;
+        // this.#cal -= topping.cal;
 
+        // return this.topping;
+    }
+
+            
+    calculatePrice() {
+        let price = this.coast.reduce((acc, item) => acc + item, 0);
+        return price + ' tug';
+    }
+
+    calculateCalories() {
+        let calories = this.cal.reduce((acc, item) => acc + item, 0);
+        return calories + ' cal';
+    }
+    
 }
 
+let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log('Price: ' + hamburger.calculatePrice());
+console.log('Calories: ' + hamburger.calculateCalories());
 
-let hamburger = new Hamburger(SIZE_SMALL, STUFFING_CHEESE);
-hamburger.addTopping(TOPPING_MAYO);
-hamburger.addTopping(TOPPING_SPICE);
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+console.log('Price: ' + hamburger.calculatePrice());
+console.log('Calories: ' + hamburger.calculateCalories());
+
+// hamburger.removeTopping(Hamburger.TOPPING_MAYO);
+// hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+
+// console.log('Price: ' + hamburger.calculatePrice());
+// console.log('Calories: ' + hamburger.calculateCalories());
+
+// console.log(`Coast: ${hamburger.coast}`, `Calories: ${hamburger.cal}`);
+
 console.log(hamburger);
-console.log(`Calories: ${hamburger.coast}`, `Coast: ${hamburger.cal}`);
-// hamburger.removeTopping(TOPPING_SPICE);
-console.log(`Calories: ${hamburger.coast}`, `Coast: ${hamburger.cal}`);
 
 
