68092ab512db5a94c55d4c9693cd1f1d6bbfb728
Anrey <andrey@example.com>
2023-01-27 04:31:00 +0200

    first commit

---
+++ dz_29_smileApp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import SmileList from './components/SmileList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <SmileList />
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
